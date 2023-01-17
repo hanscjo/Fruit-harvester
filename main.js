81cfc4f2dc1e329f4d5ad591211f476af3268987
abir314 <121821310+abir314@users.noreply.github.com>
2023-01-17 13:02:20 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let epler = Math.floor(Math.random() * 1000000);
-var pærer = Math.floor(Math.random() * 1000000);
-var bananer = Math.floor(Math.random() * 1000000);
+let pærer = Math.floor(Math.random() * 1000000);
+let bananer = Math.floor(Math.random() * 1000000);
 
 function antallFrukt(fruktNavn) {
 console.log(epler);
