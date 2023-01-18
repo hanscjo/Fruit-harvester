9c48f01c6120b6e1ae54dbf2b552e9a384b5b8ec
hanscjo <johnsen.hans.christian@gmail.com>
2023-01-18 13:47:45 +0100

    la inn fruktknapp-feedback

--- pluss.js
+++ pluss.js
@@ -10,4 +10,39 @@ function leggTilFrukt(frukt) {
             bananer++;
             break;
     }
+    fruktFeedback(frukt);
+}
+
+function fruktFeedback(frukt) {
+    document.getElementById("fruktMelding").classList.remove("feedback");
+    document.getElementById("fruktMelding").classList.add("feedback"); //Setter på animasjon
+    
+    tellFrukt(frukt);
+    if (countOfCurrentFruit == 1) {
+        oppdaterFruktMelding(`Du la oppi ${countOfCurrentFruit} ${frukt}`);
+    }
+    else {
+        oppdaterFruktMelding(`Du la oppi ${countOfCurrentFruit} ${frukt}r`)
+    }
+
+    setTimeout(fjernFeedbackClass, 400, "fruktMelding"); //Dette er nødvendig for å få animasjonen til å kjøre neste gang den trykkes 
+}
+
+function tellFrukt(frukt) { //Dette er altså bare "midlertidig telling" for å se hvor mange av samme frukt som blir lagt inn av gangen
+    if (frukt == currentFruit) {
+        countOfCurrentFruit++;
+    }
+    else {
+        countOfCurrentFruit = 1;
+        currentFruit = frukt;
+    }
+}
+
+function oppdaterFruktMelding(tekst) {
+    document.getElementById("fruktMelding").innerHTML = tekst;
+}
+
+function fjernFeedbackClass(element) {
+    document.getElementById(element).classList.remove("feedback");
+    console.log("fjerna class");
 }
