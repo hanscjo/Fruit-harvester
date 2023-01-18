56e70f920d83de2bae6caf493ffd383e48cf3175
abir314 <121821310+abir314@users.noreply.github.com>
2023-01-18 12:28:13 +0100

    main js
    fjern noe variabler (epler.......)

--- sekk.js
+++ sekk.js
@@ -1,6 +1,6 @@
-epler = Math.floor(Math.random() * 1000000);
-pærer = Math.floor(Math.random() * 1000000);
-bananer = Math.floor(Math.random() * 1000000);
+let epler = Math.floor(Math.random() * 1000000);
+let pærer = Math.floor(Math.random() * 1000000);
+let bananer = Math.floor(Math.random() * 1000000);
 function viseHide(){
     document.getElementById('vise').innerHTML = `
     <button onclick="antallFrukt(epler)">epler</button>
